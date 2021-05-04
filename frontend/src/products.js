937b7f29b39d20cdc5cb2aef9109c6aca81b3875
sucharita05 <68145021+sucharita05@users.noreply.github.com>
2021-05-04 12:22:52 +0530

    initial commit

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Kraft Seeds Garden Fork',
+    image: '/images/garden_fork.jpg',
+    description:
+      'Garden forks can dig into dense soil better than a spade.It is useful for scooping mulch or turning compost piles, much like a pitchfork',
+    brand: 'Kraft Seeds',
+    category: 'Tools',
+    price: 199,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'TrustBasket Reusable,Heavy Duty Garden Hand Gloves',
+    image: '/images/glove.jpg',
+    description:
+      'Package Contents: 2-Pieces Gardening Gloves. Ergonomically designed for greater comfort. Polyester cotton seamless liner.Lavish breathable open back, Crinkle finish for excellent grip',
+    brand: 'TrustBasket',
+    category: 'Tools',
+    price: 209,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'TrustBasket Garden Hand Trowel',
+    image: '/images/hand_trowel.jpg',
+    description:
+      'Hand Trowel is often used for digging up weeds.Hand Trowel can be used for moving a plant from one location to another.Light weight, Rust preventive coating',
+    brand: 'TrustBasket',
+    category: 'Tools',
+    price: 276,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'TAD SOLID® 202 Grade Stainless Steel Hoe',
+    image: '/images/hoe.jpg',
+    description:
+      'Agriculture Tool Primarily Used for Digging. It Contain a Blade Attached with a Long Handle. The Handle is Very Strong & Spade were Made with Sharper Tips of Metal. Heavy Duty Gardening Spade with Strong Wooden Handle Suitable for digging, balancing, forming hard/raw ground soil.',
+    brand: 'TAD SOLID',
+    category: 'Tools',
+    price: 1699,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Wolf-Garten Bypass Secateur/Garden Shear RR-EN Cards',
+    image: '/images/lopper.jpg',
+    description:
+      'Single handed locking device. Suitable for both, left and right handed users. Non stick blade, gloss galvanised. Cutting performance: up to 19 mm',
+    brand: 'Wolf-Garten',
+    category: 'Tools',
+    price: 1399,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'TrustBasket Garden Watering Can (Green 5L)',
+    image: '/images/watering_can.jpg',
+    description:
+      'Product dimensions: Length: 48.26 Cm, Width: 12.7 Cm, Height: 25.4 cm. Material: Plastic. Capacity: 5 Litres. By using our watering can, you can water upto 5 plants at a time. It is a very useful product for your gardening tool collection.',
+    brand: 'TrustBasket',
+    category: 'Tools',
+    price: 399,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
